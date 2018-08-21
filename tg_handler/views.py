7faa29d15227adfda8adc101ce53183b5123cdd1
Ali Tou <alitoumob@gmail.com>
2018-08-22 00:48:18 +0430

    Authorization of Telegram moved into a separate app.

--- tg_handler/views.py
+++ tg_handler/views.py
@@ -32,4 +32,61 @@ def send_tg_message(request):
         # token = request.POST.get('tg_token', None)
         # if token is not None:
 
-        
+# @api_view(['POST'])
+# @permission_classes((permissions.IsAuthenticated,))
+# @authentication_classes((authentication.JSONWebTokenAuthentication,))
+# def add_telegram_channel(request):
+#     channel_pk = request.data.get('channel_id', None)
+#     token = request.data.get('tg_token', None)
+#     if None in [channel_pk, token]:
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
+#     channel = Channel.objects.get(pk=channel_pk)
+#     channel.tg_token = token
+#     channel.save()
+#     serializer = ChannelSerializer(channel)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def get_channel(pk=None):
+    try:
+        channel = Channel.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        return None
+    return channel
+
+
+class TelegramAuth(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.JSONWebTokenAuthentication,)
+
+    def post(self, request):
+        channel_pk = request.data.get('channel_id', None)
+        token = request.data.get('tg_token', None)
+        if None in [channel_pk, token]:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        channel = get_channel(pk=channel_pk)
+        channel.tg_token = token
+        channel.save()
+        serializer = ChannelSerializer(channel)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get(self, request):
+        channel_pk = request.data.get('channel_id', None)
+        channel = get_channel(pk=channel_pk)
+        token = channel.tg_token
+        if token is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({'token': token}, status=status.HTTP_200_OK)
+
+    def update(self, request):
+        return self.post(request)
+
+    def delete(self, request):
+        channel_pk = request.data.get('channel_id', None)
+        if channel_pk is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        channel = get_channel(pk=channel_pk)
+        channel.token = None
+        channel.save()
+        return Response(status=status.HTTP_200_OK)
