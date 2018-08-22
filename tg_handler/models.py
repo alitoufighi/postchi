9efad2253e609d69420a43dd8c38178067a71e52
Ali Tou <alitoumob@gmail.com>
2018-08-22 20:56:22 +0430

    username field added for showing in profile

--- tg_handler/models.py
+++ tg_handler/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 class TelegramPlatform(models.Model):
+    username = models.CharField(max_length=100, blank=True)  # username of Telegram channel for showing in profile
     bot_token = models.CharField(max_length=100, blank=True)  # Token given by @BotFather in Telegram
-    # chat_ids = models.ForeignKey(TelegramChatID, on_delete=models.CASCADE, blank=True, related_name='tg_chat_ids')
-    #  TODO: FOREIGN KEY ON CHARFIELD DOESNT WORK!
-    chat_id = models.CharField(max_length=100,blank=True)
+    chat_id = models.CharField(max_length=100, blank=True)  # Numerical chat_id of Telegram channel
 
     def __str__(self):
         return self.bot_token
