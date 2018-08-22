8fa424bca7a01bc5808245e5cbfb8dcb6f8a4859
Ali Tou <alitoumob@gmail.com>
2018-08-22 21:03:37 +0430

    adding a url dispatcher check email available function.

--- postchiapp/urls.py
+++ postchiapp/urls.py
@@ -4,9 +4,9 @@ import rest_framework_jwt.views
 
 urlpatterns = [
     url(r'^signup$', views.AccountSignup.as_view(), name='signup'),
+    url(r'^check_email', views.check_email_available, name='check_email'),
     url(r'^create_channel$', views.CreateChannel.as_view(), name='create_channel'),
     url(r'^channels$', views.ListMyChannels.as_view(), name='channels_list'),
-    url(r'^login$', rest_framework_jwt.views.obtain_jwt_token),
+    url(r'^login$', rest_framework_jwt.views.obtain_jwt_token, name='login'),
     url(r'^current_user$', views.get_current_user, name='get_current_user'),
-    # url(r'^set_tg_token$', views.add_telegram_channel, name='set_tg_channel'),
 ]
