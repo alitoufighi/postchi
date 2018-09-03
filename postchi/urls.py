50c69ffddfc0cc0fafbe8e4235f2cc0c96c9fbda
Ali Tou <alitoumob@gmail.com>
2018-09-03 19:45:10 +0430

    adding media url pattern.

--- postchi/urls.py
+++ postchi/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
@@ -22,3 +24,5 @@ urlpatterns = [
     path('', include('postchiapp.urls')),
     path('post/', include('post.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # for media files
