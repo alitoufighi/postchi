f48eff63300157e335d9ab84b682a63efe3d2ba4
Ali Tou <alitoumob@gmail.com>
2018-09-03 19:44:18 +0430

    splitting PostSerializer into two serializers for Listing and Creating.

--- post/serializers.py
+++ post/serializers.py
@@ -3,19 +3,46 @@ from rest_framework.fields import CurrentUserDefault, SkipField
 from post.models import Post
 from collections import OrderedDict
 from rest_framework.relations import PKOnlyObject
+# from postchiapp import serializers as core_serializers
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostCreateSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
+    def to_representation(self, instance):
+        """
+        Object instance -> Dict of primitive datatypes.
+        """
+        ret = OrderedDict()
+        fields = self._readable_fields
+
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            # KEY IS HERE:
+            if attribute in [None, '']:
+                continue
+
+            check_for_none = attribute.pk if isinstance(attribute, PKOnlyObject) else attribute
+            if check_for_none is None:
+                ret[field.field_name] = None
+            else:
+                ret[field.field_name] = field.to_representation(attribute)
+
+        return ret
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'author', 'channel')
+        fields = ('text', 'author', 'channel', 'media')
 
 
-class PostViewSerializer(serializers.ModelSerializer):
+class PostListSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True, slug_field='username')
     channel = serializers.SlugRelatedField(read_only=True, slug_field='channel_username')
+    # channel = core_serializers.ChannelSerializer()
 
     tw_link = serializers.CharField(required=False)
     tg_link = serializers.CharField(required=False)
@@ -38,11 +65,6 @@ class PostViewSerializer(serializers.ModelSerializer):
             if attribute in [None, '']:
                 continue
 
-            # We skip `to_representation` for `None` values so that fields do
-            # not have to explicitly deal with that case.
-            #
-            # For related fields with `use_pk_only_optimization` we need to
-            # resolve the pk value.
             check_for_none = attribute.pk if isinstance(attribute, PKOnlyObject) else attribute
             if check_for_none is None:
                 ret[field.field_name] = None
@@ -51,16 +73,7 @@ class PostViewSerializer(serializers.ModelSerializer):
 
         return ret
 
-    def to_internal_value(self, data):
-        if data.get('tg_link', None) == '':
-            data.pop('tg_link')
-        if data.get('tw_link', None) == '':
-            data.pop('tw_link')
-        if data.get('insta_link', None) == '':
-            data.pop('insta_link')
-        return super(PostViewSerializer, self).to_internal_value(data)
-
     class Meta:
         model = Post
-        # fields = '__all__'
-        fields = ('text', 'author', 'channel', 'tg_link', 'tw_link', 'insta_link')
+        fields = ('id', 'text', 'author', 'channel', 'media', 'tg_link', 'tw_link', 'insta_link')
+        # depth = 1
