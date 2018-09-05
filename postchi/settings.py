1d6634724d74659ebaa72b239ba2c48d7a964eb7
Ali Tou <alitoumob@gmail.com>
2018-09-05 12:02:53 +0430

    set db to sqlite3 for pythonanywhere.

--- postchi/settings.py
+++ postchi/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'postchi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_URI"),
-        'USER': os.getenv("DB_USERNAME"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("HOST"),
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("DB_URI"),
+#         'USER': os.getenv("DB_USERNAME"),
+#         'PASSWORD': os.getenv("DB_PASSWORD"),
+#         'HOST': os.getenv("HOST"),
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
