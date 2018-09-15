6ea02375007686da7e61faed7afdea0ad5066c38
Mohammad Ali Toufighi <alitoumob@gmail.com>
2018-09-15 19:30:28 +0430

    removing channel serializer and moving to its app

--- postchiapp/serializers.py
+++ postchiapp/serializers.py
@@ -8,41 +8,6 @@ from rest_framework.relations import PKOnlyObject
 # from tg_handler.serializers import TelegramPlatformSerializer
 
 
-class ChannelSerializer(serializers.ModelSerializer):
-    owner = serializers.PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
-    # tg = TelegramPlatformSerializer()
-    # TODO: NESTED SERIALIZER?!
-
-    def to_representation(self, instance):
-        """
-        Object instance -> Dict of primitive datatypes.
-        """
-        ret = OrderedDict()
-        fields = self._readable_fields
-
-        for field in fields:
-            try:
-                attribute = field.get_attribute(instance)
-            except SkipField:
-                continue
-
-            # KEY IS HERE:
-            if attribute in [None, '']:
-                continue
-
-            check_for_none = attribute.pk if isinstance(attribute, PKOnlyObject) else attribute
-            if check_for_none is None:
-                ret[field.field_name] = None
-            else:
-                ret[field.field_name] = field.to_representation(attribute)
-
-        return ret
-
-    class Meta:
-        model = Channel
-        fields = ('id', 'name', 'channel_username', 'owner', 'tg', 'tw', 'insta',)
-
-
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
